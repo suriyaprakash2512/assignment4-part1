90740b9a5637220ad7f8f404f2b33b21207bab39
suriyaprakash2512 <suriyaprakash.r@mphasis.com>
2023-11-09 14:34:08 +0530

    assignment4-part1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4_part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "suriyaprakash.R";
+            int age = 22; 
+            double height = 168.5; 
+
+        
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age} years");
+            Console.WriteLine($"Height: {height} cm");
+
+            Console.ReadKey();
+        }
+    }
+}
